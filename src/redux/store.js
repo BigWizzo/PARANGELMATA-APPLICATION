6d9c93d90e67fe91943f1e6a4da052bb2ae9e1f6
bigwizzo <wnnwillard@gmail.com>
2021-05-15 18:48:14 +0200

    Implement redux-persit to save data to localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,19 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(
-  rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk)),
 );
 
-export default store;
+export const persistor = persistStore(store);
