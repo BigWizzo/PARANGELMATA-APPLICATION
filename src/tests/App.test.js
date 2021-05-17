68cad4e823812ea25d55b861d0997c9ae0ab445d
bigwizzo <wnnwillard@gmail.com>
2021-05-17 02:54:30 +0200

    Test the App.js component

---
+++ src/tests/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
+import { Provider } from 'react-redux';
+import { store, persistor } from '../redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+test('should render the Calculator Page', () => {
+  const component = renderer.create(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
